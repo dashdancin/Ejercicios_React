09b9e783514d0f81096d0bdf8f092706e52f3d86
dashdancing <omar.gbet@gmail.com>
2020-05-08 22:04:03 -0500

    Song Search complete

--- src/Components/SongSearch.js
+++ src/Components/SongSearch.js
@@ -11,24 +11,30 @@ const SongSearch = () => {
  const [loading, setLoading] = useState(false);
 
  useEffect(() => {
-   if (search === null) return;
-   
-   const fetchData = async () => {
-     const{ artist, song } = search;
+  if (search === null) return;
 
-     let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`
-     let songUrl= `https://api.lyrics.ovh/v1/${artist}/${title}`
+  const fetchData = async () => {
+   const { artist, song } = search;
 
-     console.log(artistUrl, songUrl);
+   let artistUrl = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+   let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
 
-     setLoading(true);
+   //  console.log(artistUrl, songUrl);
 
-     const[artistRes, songRes] = await Promise.all[
-       helpHttp().get(artistUrl),
-       helpHttp().get(songUrl),
-     ]);
-   }
- }
+   setLoading(true);
+
+   const [artistRes, songRes] = await Promise.all([
+    helpHttp().get(artistUrl),
+    helpHttp().get(songUrl),
+   ]);
+
+   setBio(artistRes);
+   setLyric(songRes);
+   setLoading(false);
+  };
+
+  fetchData();
+ }, [search]);
 
  const handleSearch = (data) => {
   // console.log(data);
