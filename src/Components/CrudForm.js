43a7f1f31956c1d8253de6433e18e9d1d46f7691
dashdancing <omar.gbet@gmail.com>
2022-05-09 01:04:18 -0500

    Jon Mircha code trick

--- src/Components/CrudForm.js
+++ src/Components/CrudForm.js
@@ -1,48 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState} from "react";
 
-const initialForm = {
-    name: '',
-    constellation: '',
-    id: null,
+const initailForm = {
+  name: "",
+  constellation: "",
+  id: null,
 };
 
 const CrudForm = () => {
-    const {form, setForm} = useState({ initialForm });
+  const [form, setForm] = useState(initailForm);
 
-    const handleChange = (e) => {}
+  const handleChange = (e) => { };
 
-    const handleSubmit = (e) => {}
+  const handleSubmit = (e) => { };
 
-    const handleReset= (e) => {}    
+  const handleReset = (e) => { };
 
   return (
-  <div>
-    <h3>Agregar</h3>
+    <div>
+      <h3>Agregar</h3>
       <form onSubmit={handleSubmit}>
-        <input 
-          type='text' 
-          name='name' 
-          placeholder='Nombre' 
+        <input
+          type="text"
+          name="name"
+          placeholder="Nombre"
           onChange={handleChange}
           value={form.name}
         />
-        <input 
-          type='text' 
-          name='constellation' 
-          placeholder='Constlacion' 
+        <input
+          type="text"
+          name="constellation"
+          placeholder="Constelación"
           onChange={handleChange}
           value={form.constellation}
-        />    
-        <input 
-          type='submit' 
-          value='Enviar'
-        />    
-        <input 
-          type='reset' 
-          value='Limpiar' 
-          onClick={handleReset} 
-        />    
-      </form>  
+        />
+        <input type="submit" value="Enviar" />
+        <input type="reset" value="Limpiar" onClick={handleReset} />
+      </form>
     </div>
   );
 };
