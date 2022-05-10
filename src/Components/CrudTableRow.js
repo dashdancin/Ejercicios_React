b7d07bb0755a0aeed37d7fa8864cb86d877fb34c
dashdancing <omar.gbet@gmail.com>
2022-05-09 23:14:28 -0500

    Button onclick id, el

--- src/Components/CrudTableRow.js
+++ src/Components/CrudTableRow.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const CrudTableRow = ({ el }) => {
+const CrudTableRow = ({ el, setDataToEdit, deleteData }) => {
+ let { name, constellation, id } = el;
  return (
   <tr>
-   <td>{el.name}</td>
-   <td>{el.constellation}</td>
+   <td>{name}</td>
+   <td>{constellation}</td>
    <td>
-    <button>Editar</button>
-    <button>Eliminar x</button>
+    <button onClick={() => setDataToEdit(e)}>Editar</button>
+    <button onClick={() => deleteData(id)}>Eliminar x</button>
    </td>
   </tr>
  );
