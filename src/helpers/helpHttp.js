20e2440e8dbc6bef49744a6fa0f75266152a3fbf
dashdancing <omar.gbet@gmail.com>
2022-05-11 09:21:04 -0500

    Helpers work!

--- src/helpers/helpHttp.js
+++ src/helpers/helpHttp.js
@@ -18,38 +18,40 @@ export const helpHttp = () => {
   //console.log(options);
   setTimeout(() => controller.abort(), 3000);
 
-  return fetch(endpoint, options).then((res) =>
-   res.ok
-    ? res.json()
-    : Promise.reject({
-       err: true,
-       status: res.status || '00',
-       statusText: res.statusText || 'Ocurrio un error',
-      })
-  );
-    .catch((err) => err);
+  return fetch(endpoint, options)
+   .then((res) =>
+    res.ok
+     ? res.json()
+     : Promise.reject({
+        err: true,
+        status: res.status || '00',
+        statusText: res.statusText || 'Ocurrió un error',
+       })
+   )
+   .catch((err) => err);
  };
- const get = (url, options = {})=> customFetch(url, options);
+
+ const get = (url, options = {}) => customFetch(url, options);
 
  const post = (url, options = {}) => {
-   options.method = 'POST';
-   return customFetch(url, options);
+  options.method = 'POST';
+  return customFetch(url, options);
  };
 
  const put = (url, options = {}) => {
-   options.method = 'PUT';
-   return customFetch(url, options);
+  options.method = 'PUT';
+  return customFetch(url, options);
  };
 
  const del = (url, options = {}) => {
-   options.method = 'DELETE';
-   return customFetch(url, options);
+  options.method = 'DELETE';
+  return customFetch(url, options);
  };
 
  return {
-   get, 
-   post,
-   put,
-   del,
+  get,
+  post,
+  put,
+  del,
  };
 };
