f2aeb652b119c22fe5647d9516b9fa0b4123df92
dashdancing <omar.gbet@gmail.com>
2022-05-12 22:44:00 -0500

    Featura implement setLoading component

--- src/Components/SongArtist.js
+++ src/Components/SongArtist.js
@@ -1,7 +1,24 @@
-import React from 'react';
-
-const SongArtist = () => {
- return <div>Song Artist</div>;
+const SongArtist = ({ artist }) => {
+ return (
+  <section>
+   <h3>{artist.strArtist}</h3>
+   <img src={artist.strArtistThumb} alt={artist.strArtist} />
+   <p>
+    {artist.intBornYear} - {artist.intDiedYear || 'Presente'}
+   </p>
+   <p>
+    {artist.strGenre} - {artist.strStyle}
+   </p>
+   <a
+    href={`http://${artist.strWebsite}`}
+    target='_blank'
+    rel='noreferrer'
+   >
+    Sitio Web Oficial
+   </a>
+   <p>{artist.strBiographyEN}</p>
+  </section>
+ );
 };
 
 export default SongArtist;
