21dad65bf0b7301d13cf128aeb9dfa599fc52ccb
dashdancing <omar.gbet@gmail.com>
2022-05-12 23:01:37 -0500

    Refactor SongDetails

--- src/Components/SongDetails.js
+++ src/Components/SongDetails.js
@@ -3,6 +3,10 @@ import SongArtist from './SongArtist';
 import SongLyric from './SongLyric';
 
 const SongDetails = ({ search, lyric, bio }) => {
+  if (!lyric || lyric.err || lyric.name === 'AbortError' ? (
+    <Message 
+    msg={ `Error: np existe la canción '<em>${search.song}</em>`}}
+  ))
  return (
   <div>
    <h2>Detalles</h2>
