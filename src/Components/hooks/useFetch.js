ab2f10cad991fff91e8c6be635fa5744e6923ecf
dashdancing <omar.gbet@gmail.com>
2022-05-14 21:57:48 -0500

    Conditional error

--- src/Components/hooks/useFetch.js
+++ src/Components/hooks/useFetch.js
@@ -12,7 +12,25 @@ export const useFetch = (url) => {
   const fetchData = async () => {
    setLoading(true);
    try {
-     const res = await fetch(url, {signal})
+    const res = await fetch(url, { signal });
+
+    if (!res.ok) {
+     let err = new Error('Error en la petición Fetch');
+     err.status = res.status || '00';
+     err.statusText = res.statusText || 'Ocurrió un error';
+     throw err;
+    }
+    const json = await res.json();
+
+    if (!signal.aborted) {
+     setData(json);
+     setError(null);
+    }
+   } catch (error) {
+    if (!signal.aborted) {
+     setData(null);
+     setError(error);
+    }
    }
   };
 
